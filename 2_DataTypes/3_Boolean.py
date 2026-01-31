b22cb23825b44fc2f784002006c5d40efec3adcf
MASUM BILLAH <masumb2k2@gmail.com>
2026-01-31 22:59:05 +0600

    Add Boolean data type implementation with user login status and item availability checks

---
+++ 2_DataTypes/3_Boolean.py
@@ -0,0 +1,19 @@
+# Boolean = True, False
+
+is_user_logged_in = True
+print(f"User Login status: {is_user_logged_in}")
+
+shop_current_item_amount =150
+is_cookies_have = True
+
+total_item_amount = shop_current_item_amount + is_cookies_have
+
+print(f"Item Amount: {total_item_amount}")
+
+# Operator : And , or , Not 
+
+is_sugar_available = True
+is_honey_available = False
+
+print(f"Is User Served : {is_sugar_available and is_honey_available}")
+print(f"Is User Served : {is_sugar_available or is_honey_available}")
