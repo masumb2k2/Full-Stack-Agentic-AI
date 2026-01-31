a9c0c3087a0e252f65ac534747bc6c9ac3abc0ba
MASUM BILLAH <masumb2k2@gmail.com>
2026-01-31 23:05:55 +0600

    Add implementation for decimal number temperature difference calculation

---
+++ 2_DataTypes/4_real_number.py
@@ -0,0 +1,9 @@
+# Decimal number
+import sys
+ideal_temperature = 98.6
+current_temp =98.553646894599345947569034586095678578465476974536973459876
+
+difference = ideal_temperature - current_temp
+
+print(f"Tempareture Difference: {current_temp -ideal_temperature}")
+print(sys.float_info)
