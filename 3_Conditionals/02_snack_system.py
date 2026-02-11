d54b2ece873181952962f8eb87f7ddf7c02daf23
MASUM BILLAH <masumb2k2@gmail.com>
2026-02-11 22:50:20 +0600

    Add snack selection functionality with confirmation message

---
+++ 3_Conditionals/02_snack_system.py
@@ -0,0 +1,6 @@
+snack_choice = input("Enter Your Choice(cookies/samosa):").lower()
+
+if snack_choice == 'cookies' or 'samosa':
+    print('Great choice! Order Confirmed.')
+else:
+    pritn('Sorry! Item not available')
