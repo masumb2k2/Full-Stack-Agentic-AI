e90035d3650dd557238d6248a0c279fc5d22d672
MASUM BILLAH <masumb2k2@gmail.com>
2026-02-14 23:21:47 +0600

    Splitting complex task using function

---
+++ 05_Functions/02_generate_monthly_report.py
@@ -0,0 +1,16 @@
+def fetch_sales():
+    print("Fetching Sales data")
+
+def filtered_valid_orders():
+    print('Filtering Order')
+
+def summarize_data():
+    print("Summarizing date")
+
+def geenrate_report():
+    fetch_sales()
+    filtered_valid_orders()
+    summarize_data()
+    print("Report Generated")
+
+geenrate_report()
