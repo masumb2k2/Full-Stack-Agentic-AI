6f2562bc00447a5f157a5e74040fc9a95aba3a56
MASUM BILLAH <masumb2k2@gmail.com>
2026-02-21 22:44:48 +0600

    Hide implementation

---
+++ 05_Functions/03_hiding_implementation_details.py
@@ -0,0 +1,16 @@
+def get_input():
+    print("getting user input")
+
+def validate_input():
+    print("Validating Input value")
+
+def save_to_db():
+    print("Saving to Db")
+
+def register_user():
+    get_input()
+    validate_input()
+    save_to_db()
+    print("User Registered")
+
+register_user()
