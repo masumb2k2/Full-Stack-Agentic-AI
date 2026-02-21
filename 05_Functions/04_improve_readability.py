bc585129c18a00dd18fbd940a8459d4fb216f7fb
MASUM BILLAH <masumb2k2@gmail.com>
2026-02-21 22:49:57 +0600

    Improve readbility

---
+++ 05_Functions/04_improve_readability.py
@@ -0,0 +1,8 @@
+def calculate_bill(cups,price_per_cup):
+    return cups*price_per_cup
+
+my_bill = calculate_bill(5,15)
+print(f"My Bill is : {my_bill}")
+
+oishis_bill = calculate_bill(13,4)
+print(f"oishi's Bill is : {oishis_bill}")
