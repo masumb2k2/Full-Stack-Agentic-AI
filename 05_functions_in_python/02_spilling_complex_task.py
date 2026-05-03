bd92ef18177b6c6e2ed583b716c25a68e2c1de86
MASUM BILLAH <masumb2k2@gmail.com>
2026-05-03 17:14:15 +0600

    Create 02_spilling_complex_task.py

---
+++ 05_functions_in_python/02_spilling_complex_task.py
@@ -0,0 +1,16 @@
+def fetch_sales():
+    print("Feching Sales Data")
+
+def validate_sales_data():
+    print("Validating Sales Data")
+
+def summarize_data():
+    print("summarizing sales data")
+
+def generate_report():
+    fetch_sales()
+    validate_sales_data()
+    summarize_data()
+    print("Report generated")
+
+generate_report()
