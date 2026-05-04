43749908c11030fee6e20839b877ac72b9531439
MASUM BILLAH <masumb2k2@gmail.com>
2026-05-04 22:48:26 +0600

    Create 03_hiding_implementaion_task.py

---
+++ 05_functions_in_python/03_hiding_implementaion_task.py
@@ -0,0 +1,16 @@
+def user_input():
+    print("getting User input")
+
+def validate_input():
+    print("Validating input")
+
+def save_to_db():
+    print("saving to db")
+
+def register_user():
+    user_input()
+    validate_input()
+    save_to_db()
+    print("register")
+
+register_user()
