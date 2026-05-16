ef179eb026fd902b0315e2229b7ed98414f1eca1
MASUM BILLAH <masumb2k2@gmail.com>
2026-05-16 23:50:25 +0600

    Create 03_mutable.py

---
+++ 01_DataTypes/03_mutable.py
@@ -0,0 +1,18 @@
+# print(available_food_item)
+# print(f"calucation {1+2}")
+
+available_food_item = 3
+print(f"Amader Shop e item ache: {available_food_item} ti")
+print(f"Item er Prothomer id {id(available_food_item)}")
+
+
+available_food_item = 20
+print(f"Amader Shop e Notun tem ache: {available_food_item} ti")
+print(f"Item er Second id {id(available_food_item)}")
+
+available_food_item = 3
+print(f"Amader Shop e Notun tem ache: {available_food_item} ti")
+print(f"Item er 3rd id {id(available_food_item)}")
+
+
+
