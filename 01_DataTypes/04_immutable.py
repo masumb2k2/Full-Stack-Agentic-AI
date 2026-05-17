5440468af0664dc712ed0d2f01c66cb1601f131d
MASUM BILLAH <masumb2k2@gmail.com>
2026-05-17 21:29:07 +0600

    Update 04_immutable.py

--- 01_DataTypes/04_immutable.py
+++ 01_DataTypes/04_immutable.py
@@ -1,8 +1,9 @@
 namer_set = set()
+namer_set.add("Sabrina")
 print(f"Prothome id {id(namer_set)}")
 print(namer_set)
 
-namer_set.add("Sabrina")
+
 namer_set.add("Lubna")
 print(f"Second id {id(namer_set)}")
 print(namer_set)
