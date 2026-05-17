dc88f17fb6b37e53451bb561c0c612d31f5c5fcf
MASUM BILLAH <masumb2k2@gmail.com>
2026-05-17 23:04:31 +0600

    Create 05_numbers.py

---
+++ 01_DataTypes/05_numbers.py
@@ -0,0 +1,35 @@
+# Numbers 
+coffee_item = 10
+new_item_added = 3
+
+# Addition 
+total_coffee_item = coffee_item + new_item_added
+print(f"Total coffee item after add: {total_coffee_item}")
+
+#Substracion 
+total_item_after_subtraction = coffee_item - 5
+print(f"After subtraction: {total_item_after_subtraction}")
+
+# Multiplication - * 
+
+# Division 
+coffee_cup = 13
+customer = 3
+
+coffee_for_each_customer = coffee_cup / customer
+print(f"Coffee for each Customer: {coffee_for_each_customer}")
+
+absulate_coffee_cup = coffee_cup // customer
+print(f"absulate Coffee for each Customer: {absulate_coffee_cup}")
+
+# Remainder = ভাগশেষ 
+lefover_cup = coffee_cup % customer
+print(f"Lefover cup : {lefover_cup}")
+
+# 2^3
+exponational = 2 ** 3
+print(f"Exponential: {exponational}")
+
+# Larger number 
+taka_ache = 1_000_000_00_000 # Improve the Readbility
+print(f"Amar kache taka ache : {taka_ache}")
