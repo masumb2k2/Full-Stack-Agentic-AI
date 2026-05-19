4671ac64757226915a036172c9df1da2e7691f1d
MASUM BILLAH <masumb2k2@gmail.com>
2026-05-19 22:54:59 +0600

    Create viva_01.py

---
+++ 01_DataTypes/viva_01.py
@@ -0,0 +1,20 @@
+# 1- oishi 2- sabrina 3- himely 4- mehtaz
+
+# Lubna --> 4.5 + 2
+# oishi --> 3 + 5
+# Himely --> 5 + 1
+
+my_string = "Bangladesh is Beatiful Country"
+
+print(my_string[1::3])
+
+# 1.
+print(f"want to get {my_string[15:23]}")
+
+# amar kache 30 ta amm ache 7 jonke dilam koita obosisto
+total_amm = 30
+peoples = 7
+remaider = total_amm % peoples
+print(remaider)
+
+print(f"remainder = {total_amm%peoples}")
