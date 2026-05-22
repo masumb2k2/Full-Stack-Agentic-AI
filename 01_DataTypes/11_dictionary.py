71cf22ca50d6ec4855bdae67db1dfcde8ff10f5c
MASUM BILLAH <masumb2k2@gmail.com>
2026-05-22 23:11:08 +0600

    Create 11_dictionary.py

---
+++ 01_DataTypes/11_dictionary.py
@@ -0,0 +1,52 @@
+# Dictionary is nothing but key value pair
+
+# name (KEY) = masum (Value)
+#  Key & value pair is called as Item
+registered_customer = {
+    'name': 'john',
+    'age': 24,
+    'address': 'Dhaka'
+}
+
+# Entire dictionary Call
+print(f"Registered Customer: {registered_customer}")
+# Value Retrive
+print(f"Customer Name: {registered_customer['name']}")
+# Add new item 
+registered_customer['Gender'] = 'Male'
+print(f"Registered Customer after add: {registered_customer}")
+
+# update value
+registered_customer['age'] = 30
+print(f"Registered Customer after update: {registered_customer}")
+
+# delete item 
+del registered_customer['address']
+print(f"Registered Customer after delete addresss: {registered_customer}")
+
+# membershipm testing
+print(f"class existence in customer :{'class' in registered_customer}")
+
+# Only key finding 
+print(f"only keys: {registered_customer.keys()}")
+print(f"only value: {registered_customer.values()}")
+print(f"only items: {registered_customer.items()}")
+
+# pop item 
+last_item = registered_customer.popitem()
+print(f"Popped item: {last_item}")
+
+# update with another dictionary
+another_dictionary ={
+    'class' : 13, 
+    'dept' : 'CSE'
+}
+
+registered_customer.update(another_dictionary)
+print(f"Updated Customer List : {registered_customer}")
+
+# Value fetching
+# print(f"Customer Name: {registered_customer['university']}")
+
+university_data = registered_customer.get('university', 'Not found')
+print(f"Customer Name: {university_data}")
