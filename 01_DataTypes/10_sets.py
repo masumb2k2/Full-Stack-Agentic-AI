27da3b5be897ae768a8215b6593479e0ab0173c1
MASUM BILLAH <masumb2k2@gmail.com>
2026-05-22 22:42:02 +0600

    sets

---
+++ 01_DataTypes/10_sets.py
@@ -0,0 +1,24 @@
+# Collection of Unique Element 
+
+# Touple List Set 
+
+shop_item = {"Fried Rice", 'Chicken Fry', 'onthon', 'Tea', 'coffee'}
+
+new_item = {'coffee', 'biriyani', 'hot choclate', 'coffee' }
+
+print(f"New added item: {new_item}")
+
+common_item = shop_item & new_item  # union
+print(f"common items: {common_item}")
+
+all_items = shop_item | new_item # intersection
+print(f"All item in Two sets : {all_items}")
+print(f"All item in Two sets : {type(all_items)}")
+
+
+# Diffrence 
+only_in_shop_item = shop_item - new_item
+print(f"only in shop item : {only_in_shop_item}")
+
+# membership testing
+print(f"Coffee avalibility in shop item: {'Coffee' in shop_item}")
