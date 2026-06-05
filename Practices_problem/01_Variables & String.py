b3cad5780971c0ed61d6ff9d78663fd2c1a1ac39
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-05 23:24:04 +0600

    Create 01_Variables & String.py

---
+++ Practices_problem/01_Variables & String.py
@@ -0,0 +1,4 @@
+full_name = "Masum Billah"
+print(f"Name in Uppercase: {full_name.upper()}")
+print(f"Character Count in name Exclude space: {len(full_name.replace(" ",""))}")
+print(f"Is a exist in name or not: {"a" in full_name}")
