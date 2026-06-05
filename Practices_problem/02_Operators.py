706523e21f9b386d03984716ec7b576e3c0f0d00
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-05 23:30:15 +0600

    Create 02_Operators.py

---
+++ Practices_problem/02_Operators.py
@@ -0,0 +1,10 @@
+a = 17
+b = 5
+print(f"Addition of a & b: {a+b}")
+print(f"Substraction of a & b: {a-b}")
+print(f"Multiplication of a & b: {a*b}")
+print(f"Division of a & b: {a/b}")
+print(f"Modulas of a & b: {a//b}")
+print(f"Remainder of a & b: {a%b}")
+print(f"Exponential of a & b: {a**b}")
+print(f"a is greater than b AND b is not equal to 0: {a>b and b!=0}")
