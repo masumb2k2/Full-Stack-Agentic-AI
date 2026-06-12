a9645b72e709fb09a1268b271987b74822157c3e
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-12 20:46:53 +0600

    Create 03_scope_and_named_space.py

---
+++ 05_functions/03_scope_and_named_space.py
@@ -0,0 +1,19 @@
+def serve_tea():
+    tea_type = 'Masala Tea' # Local Scoe
+    print(f"Inside function : {tea_type}")
+
+tea_type = 'Lemom'
+serve_tea()
+print(f"Outside Function: {tea_type}")
+
+
+def tea_counter():
+    tea_order = 'Lemon' #Enclosing Scope
+    def print_order():
+        tea_order= 'Gingar'
+        print(f"Inner: {tea_order}")
+    print(f"Outer: {tea_order}")
+
+tea_order = 'Tulsi' # Global
+tea_counter()
+print(f"global: {tea_order}")
