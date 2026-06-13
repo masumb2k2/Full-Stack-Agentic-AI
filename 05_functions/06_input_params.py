08553fd59b445c87a125073a1ad1372d7d9c8e1a
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-13 20:04:53 +0600

    problem 1

---
+++ 05_functions/06_input_params.py
@@ -0,0 +1,13 @@
+# tea = 'Gingar Tea'
+
+# def prepare_tea(order):
+#     print(f"Prepring: {order}")
+
+# prepare_tea(tea)
+
+tea = [1,2,3]
+def edit_tea(cup):
+    cup[1]=42
+
+edit_tea(tea)
+print(tea) 
---
+++ Practices_problem/05_verify.py
@@ -0,0 +1,7 @@
+# Problem 1 — Variables & Strings
+name = 'Masum'
+age = 24
+city = 'Dhaka'
+print(f"My name is {name}, age {age}, from {city}")
+print(f"Name in uppercase : {name.upper()}")
+print(f"Number of character in name is: {len(name)}")
