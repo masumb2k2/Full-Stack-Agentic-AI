c481d725f911aa347d4276745830d795200716d1
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-13 20:11:03 +0600

    Update 05_verify.py

--- Practices_problem/05_verify.py
+++ Practices_problem/05_verify.py
@@ -5,3 +5,15 @@ city = 'Dhaka'
 print(f"My name is {name}, age {age}, from {city}")
 print(f"Name in uppercase : {name.upper()}")
 print(f"Number of character in name is: {len(name)}")
+
+# Problem 2 — Conditions
+number = int(input("Enter a number: "))
+if number>0:
+    print(f"{number} is Positive Number")
+elif number<0:
+    print(f"{number} is Negative Number")
+elif number == 0:
+    print(f"{number} is Zero")
+else:
+    print(f"{number} is invalid")
+
