201b5285aa94d0a843144a2b6d1165faccbe8a91
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-16 16:55:28 +0600

    Create 09_built_in_function.py

---
+++ 05_functions/09_built_in_function.py
@@ -0,0 +1,17 @@
+def tea_flavours(flavour = 'masala'):
+    """Return the flavour"""
+    return flavour
+
+# __dunder__
+print(tea_flavours.__doc__)
+print(tea_flavours.__name__)
+
+def generate_bill(tea = 0, samosa = 0):
+    """"
+    Calculate the total bill of tea and samosa
+    : param tea: number of tea
+    : parm: samosa : number of samosa
+    : return toal
+    """
+    total = chai*10 +samosa*15
+    return total, "Thank you"
