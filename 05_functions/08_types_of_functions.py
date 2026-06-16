fc7086a2cb309a8fa46024847195d7c090fe2b0c
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-16 14:43:50 +0600

    Create 08_types_of_functions.py

---
+++ 05_functions/08_types_of_functions.py
@@ -0,0 +1,24 @@
+def pure_tea(cups): # pure function
+    return cups *10
+
+total_tea = 0
+
+# Not Recommended
+def impure_tea(cups): # impure function
+    global total_tea
+    total_tea +=cups
+
+# Recusrsive function = call itself inside its body
+def pour_tea(n):
+    if n==0:
+        return 'All cups are pure'
+    return pour_tea(n-1) 
+
+print(pour_tea(3))
+
+
+# Lamdas Function
+tea_types = ['Light', 'kadak', 'gingar', 'kadak']
+
+strong_tea = list(filter(lambda tea: tea !='kadak',tea_types))
+print(strong_tea)
