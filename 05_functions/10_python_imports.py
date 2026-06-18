7e0531077d1816e0bd4c1042a8144f43d5d6a66a
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-18 20:37:04 +0600

    import

--- 05_functions/10_python_imports.py
+++ 05_functions/10_python_imports.py
@@ -1,1 +1,10 @@
-from recipes
+# import import_test.recipies.flavour
+# print(import_test.recipies.flavour.gingar_tea())
+
+# Named import
+from import_test.recipies.flavour import gingar_tea,elachi_tea
+print(gingar_tea())
+
+# relative Import #
+from .import_test.recipies.flavour import gingar_tea,elachi_tea
+print(elachi_tea())
