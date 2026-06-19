6c167bc5f3e774f81296f8ff9b640803b5ee4b0f
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-19 20:23:50 +0600

    Update practice.py

--- 05_functions/practice.py
+++ 05_functions/practice.py
@@ -2,9 +2,12 @@ def serve_tea():
     tea_count = 12
     print(f"Local Tea Count is :{tea_count} ")
     def print_count():
-        tea_count =5
+        # nonlocal tea_count
+        global tea_count
+        tea_count = 15
         print(f"Enclosing Tea Count: {tea_count}")
     print_count()
 
 tea_count =5
+print(tea_count)
 serve_tea()
