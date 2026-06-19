ea7bd8037f051e423d8fe143b844678cf81cc5eb
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-19 20:38:06 +0600

    Update practice.py

--- 05_functions/practice.py
+++ 05_functions/practice.py
@@ -1,13 +1,17 @@
-def serve_tea():
-    tea_count = 12
-    print(f"Local Tea Count is :{tea_count} ")
-    def print_count():
-        # nonlocal tea_count
-        global tea_count
-        tea_count = 15
-        print(f"Enclosing Tea Count: {tea_count}")
-    print_count()
+def order_datils(tea_type , cups, amount):
+    return f"Tea type {tea_type}, cups {cups}, anount {amount}"
 
-tea_count =5
-print(tea_count)
-serve_tea()
+print(order_datils('gingar',12,120))
+print(order_datils(tea_type='elachi',cups=5,amount=100))
+
+def order_items(*items, **amount):
+    return f"Iteams are {items} and amount are {amount}"
+
+print(order_items('Tea', 'cooffee', tea_amount = 12, coffee_amount = 5))
+
+# Null value handel 
+def order_values(order = None):
+    if order is None:
+        order = []
+    print(order)
+order_values([12,3,4])
