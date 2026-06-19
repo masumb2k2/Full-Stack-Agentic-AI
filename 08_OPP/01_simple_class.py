77355b8a1f4b2351140fd78e309f9402aeac3287
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-19 21:25:01 +0600

    Update 01_simple_class.py

--- 08_OPP/01_simple_class.py
+++ 08_OPP/01_simple_class.py
@@ -4,7 +4,7 @@ class TeaTime():
     pass
 print(type(Tea))
 
-gingar_tea = Tea() # class
+gingar_tea = Tea() # object
 print(type(gingar_tea))
 print(type(gingar_tea) is Tea)
 print(type(gingar_tea) is TeaTime)
