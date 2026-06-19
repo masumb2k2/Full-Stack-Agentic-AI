28f91f701307ee8316e2cbf8f2f0bd54ce6c88a2
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-19 21:24:30 +0600

    Create 01_simple_class.py

---
+++ 08_OPP/01_simple_class.py
@@ -0,0 +1,11 @@
+class Tea: # Class
+    pass
+class TeaTime():
+    pass
+print(type(Tea))
+
+gingar_tea = Tea() # class
+print(type(gingar_tea))
+print(type(gingar_tea) is Tea)
+print(type(gingar_tea) is TeaTime)
+
