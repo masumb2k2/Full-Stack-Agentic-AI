0f0f56e0c1f80c299685c918a6f612721c58ecb2
MASUM BILLAH <masumb2k2@gmail.com>
2026-06-20 11:40:16 +0600

    Create 04_self_args.py

---
+++ 08_OPP/04_self_args.py
@@ -0,0 +1,15 @@
+class Teacup:
+    size = 150 # ml
+    def describe(self):
+        return f"Cup size {self.size} ml"
+    
+cup = Teacup()
+print(cup.describe())
+
+print(Teacup.describe(cup))
+
+
+
+cup_2 = Teacup()
+cup_2.size = 100
+print(Teacup.describe(cup_2))
