e5fcb940fd10e61c063a762c13cc58ab2ca43275
MASUM BILLAH <masumb2k2@gmail.com>
2026-07-04 21:23:57 +0600

    Create 01_problem.py

---
+++ 02_condtionals/01_problem.py
@@ -0,0 +1,6 @@
+product_list = ['rice', 'soap' ,'choclatess', 'cips']
+product_prices =['100', '150', '30', '70']
+
+# [Product Name] costs ৳[Price] 
+for variable in range(len(product_list)):
+    print(f"{product_list[variable]} costs ৳{product_prices[variable]}")
