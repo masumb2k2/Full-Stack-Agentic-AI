e66985e879e200f9d04f610474d700a8a2c777cc
MASUM BILLAH <masumb2k2@gmail.com>
2026-08-05 23:08:47 +0600

    Create 08_break_continue.py

---
+++ 03_Loops/08_break_continue.py
@@ -0,0 +1,13 @@
+flavours = [
+    'green tea',
+    'masala tea',
+    'discontinue',
+    'tulsi tea', 
+    'black tea', 
+    'out of stock',
+    'coffee', 
+    'latte'
+]
+
+for flavour in flavours:
+    print(flavour)
